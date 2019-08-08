10f9940e0cecea33bcaddc2b03cb7a615b6c0f0a
Daniel Randall <dgrandall@me.com>
2019-08-08 10:10:02 -0600

    clean up a few more things

--- server.go
+++ server.go
@@ -7,36 +7,20 @@ import (
 	"github.com/byuoitav/common"
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/flight-deck/handlers"
-	"github.com/byuoitav/flight-deck/socket"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	port := ":8008"
 	router := common.NewRouter()
-	router.Pre(middleware.RemoveTrailingSlash())
-	router.Use(middleware.CORS())
 
 	// unautheticated routes
 	router.Static("/*", "public")
-	router.GET("/health", health)
 
 	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
 
 	/* secure routes */
-	// deployment
 	secure.GET("/webhook_device/:hostname", handlers.DeployByHostname)
-
-	// divider sensor contacts enable/disable
-	secure.GET("/webhook_contacts/enable/:hostname", handlers.EnableContacts)
-	secure.GET("/webhook_contacts/disable/:hostname", handlers.DisableContacts)
-
-	// websocket/ui
-	secure.GET("/ws", socket.EchoServeWS)
-
-	// TODO new pi endpoint (for showing provision number thing)
-	secure.GET("/newpi", handlers.NewPI)
 
 	err := router.StartServer(&http.Server{
 		Addr:           port,
@@ -45,8 +29,4 @@ func main() {
 	if err != nil {
 		log.L.Errorf("failed to start http server: %v", err)
 	}
-}
-
-func health(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "Did you ever hear the tragedy of Darth Plagueis The Wise?")
 }
