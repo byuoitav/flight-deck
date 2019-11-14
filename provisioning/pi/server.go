f9802223e89d370a44b9e90e1a01055ff8d417e2
Daniel Randall <dgrandall@me.com>
2019-11-14 11:10:15 -0700

    it's like actually working

--- provisioning/pi/server.go
+++ provisioning/pi/server.go
@@ -11,20 +11,36 @@ import (
 	"os/exec"
 	"path"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/labstack/echo"
 	"github.com/sparrc/go-ping"
 )
 
-type RouteData struct {
-	IPs      []string
-	Hostname string
-}
+const (
+	// HostnameFile = "/etc/hostname"
+	// DHCPFile     = "/etc/dhcpcd.conf"
+	// ResolveFile  = "/etc/resolv.conf"
 
-type IPInfo struct {
-	Interface net.Interface
-	IP        net.IP
+	HostnameFile = "./hostname"
+	DHCPFile     = "./dhcpcd.conf"
+	ResolveFile  = "./resolv.conf"
+)
+
+type RouteData struct {
+	IPs            map[string]string
+	ActualHostname string
+	Error          error
+
+	// flags
+	IgnoreSubnet bool
+	UseDHCP      bool
+
+	DesiredHostname string
+	AssignedIP      string
+
+	sync.Mutex
 }
 
 type Template struct {
@@ -35,6 +51,8 @@ var (
 	ErrNotInDNS       = errors.New("hostname not found in DNS (qip)")
 	ErrHostnameExists = errors.New("hostname is already on the network")
 	ErrInvalidSubnet  = errors.New("given ip doesn't match current subnet")
+
+	data RouteData
 )
 
 func main() {
@@ -57,7 +75,10 @@ func main() {
 	e.GET("/pages/*", func(c echo.Context) error {
 		pageName := path.Base(c.Request().URL.Path)
 
-		err := c.Render(http.StatusOK, pageName+".html", map[string]string{"Name": "Matthew Smith"})
+		data.Lock()
+		defer data.Unlock()
+
+		err := c.Render(http.StatusOK, pageName+".html", data)
 		if err != nil {
 			fmt.Printf("error rendering template %s: %v", pageName, err)
 		}
@@ -65,20 +86,67 @@ func main() {
 		return err
 	})
 
-	e.PUT("/hostname/:hostname", func(c echo.Context) error {
+	setHostnameHandler := func(c echo.Context) error {
+		data.Lock()
+		defer data.Unlock()
+
+		err := setHostname(data.DesiredHostname, data.IgnoreSubnet, data.UseDHCP)
+		switch {
+		case errors.Is(err, ErrNotInDNS):
+			fmt.Printf("redirecting to 'not in dns' page\n\n")
+			return c.Redirect(http.StatusTemporaryRedirect, "/pages/updateQIP")
+		case errors.Is(err, ErrHostnameExists):
+			fmt.Printf("redirecting to 'hostname already exists' page\n\n")
+			return c.Redirect(http.StatusTemporaryRedirect, "/pages/hostnameTaken")
+		case errors.Is(err, ErrInvalidSubnet):
+			fmt.Printf("redirecting to 'invalid subnet' page\n\n")
+			return c.Redirect(http.StatusTemporaryRedirect, "/pages/wrongSubnet")
+		case err != nil:
+			data.Error = err
+
+			fmt.Printf("redirecting to 'error' page with error: %s\n\n", data.Error)
+			return c.Redirect(http.StatusTemporaryRedirect, "/pages/error")
+		}
+
+		// redirect to success page
+		return c.Redirect(http.StatusTemporaryRedirect, "/pages/hostnameSuccess")
+	}
+
+	e.GET("/ignoreSubnet", func(c echo.Context) error {
+		if len(data.DesiredHostname) > 0 {
+			data.Lock()
+			data.IgnoreSubnet = true
+			data.Unlock()
+
+			return setHostnameHandler(c)
+		}
+
+		return c.Redirect(http.StatusTemporaryRedirect, "/pages/start")
+	})
+
+	e.GET("/useDHCP", func(c echo.Context) error {
+		if len(data.DesiredHostname) > 0 {
+			data.Lock()
+			data.UseDHCP = true
+			data.Unlock()
+
+			return setHostnameHandler(c)
+		}
+
+		return c.Redirect(http.StatusTemporaryRedirect, "/pages/start")
+	})
+
+	e.GET("/hostname/:hostname", func(c echo.Context) error {
 		hn := c.Param("hostname")
 		if len(hn) == 0 {
 			return c.String(http.StatusBadRequest, "must include a valid hostname")
 		}
 
-		err := setHostname(hn, true)
-		switch {
-		case errors.Is(err, ErrNotInDNS):
-			// redirect to please put in qip
-		case err != nil:
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
-		return c.String(http.StatusOK, "success!")
+		data.Lock()
+		data.DesiredHostname = hn
+		data.Unlock()
+
+		return setHostnameHandler(c)
 	})
 
 	// launch chomium
@@ -94,13 +162,57 @@ func main() {
 		*/
 	}()
 
+	// update current data every 10 seconds
+	go func() {
+		updateIPs := func() {
+			data.Lock()
+			defer data.Unlock()
+
+			ips, err := getIPs()
+			if err != nil {
+				fmt.Printf("failed to get current ips: %s\n", err)
+				return
+			}
+
+			// wipe out the old ip map
+			data.IPs = make(map[string]string)
+
+			for k, v := range ips {
+				data.IPs[k] = v.String()
+			}
+		}
+
+		updateHostname := func() {
+			data.Lock()
+			defer data.Unlock()
+
+			hn, err := os.Hostname()
+			if err != nil {
+				fmt.Printf("failed to get current hostname: %s\n", err)
+				return
+			}
+
+			data.ActualHostname = hn
+		}
+
+		// get initial data
+		updateIPs()
+		updateHostname()
+
+		ticker := time.NewTicker(5 * time.Second)
+		for range ticker.C {
+			updateIPs()
+			updateHostname()
+		}
+	}()
+
 	if err := e.Start(":80"); err != nil {
 		fmt.Printf("failed to start server: %s\n", err)
 		os.Exit(1)
 	}
 }
 
-//Render Meet the echo templating requirement
+// Render meets the echo templating requirement
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -152,14 +264,17 @@ func getIPs() (map[string]*net.IPNet, error) {
 	return ips, nil
 }
 
-func setHostname(hn string, ignoreSubnet bool) error {
+func setHostname(hn string, ignoreSubnet bool, useDHCP bool) error {
+	fmt.Printf("Setting hostname to %s (ignoreSubnet: %v, useDHCP: %v)\n", hn, ignoreSubnet, useDHCP)
+
 	// dns lookup new hostname
 	addrs, err := net.LookupHost(hn)
 	if err != nil {
 		return fmt.Errorf("unable to lookup host: %w", err)
 	}
 
-	if len(addrs) == 0 {
+	switch {
+	case !useDHCP && len(addrs) == 0:
 		return ErrNotInDNS
 	}
 
@@ -174,67 +289,88 @@ func setHostname(hn string, ignoreSubnet bool) error {
 		}
 	}
 
-	if ip.IP == nil {
-		// TODO some page for this case?
-		return errors.New("no suitable ip address found")
+	if ip.IP == nil && !useDHCP {
+		return ErrNotInDNS // even though it is, there must not be an ipv4 address for it
+	}
+
+	if !useDHCP {
+		fmt.Printf("Address found for %s in DNS: %s\n", hn, ip.IP.String())
 	}
 
 	// try pinging that IP
-	pinger, err := ping.NewPinger(ip.IP.String())
-	if err != nil {
-		return fmt.Errorf("unable to build pinger: %s", err)
-	}
+	if ip.IP != nil {
+		pinger, err := ping.NewPinger(ip.IP.String())
+		if err != nil {
+			return fmt.Errorf("unable to build pinger: %s", err)
+		}
 
-	pinger.Timeout = 5 * time.Second
-	pinger.Count = 3
-	pinger.Run()
+		fmt.Printf("Pinging %s...\n", ip.IP.String())
 
-	stats := pinger.Statistics()
-	if stats.PacketsRecv > 0 {
-		return ErrHostnameExists
+		pinger.Timeout = 5 * time.Second
+		pinger.Count = 3
+		pinger.Run()
+
+		stats := pinger.Statistics()
+		fmt.Printf("Received %v ping responses from %s (total loss: %v%%)\n", stats.PacketsRecv, ip.IP.String(), stats.PacketLoss)
+
+		if stats.PacketsRecv > 0 {
+			return ErrHostnameExists
+		}
 	}
 
 	// check that the ip i found works for one of the subnets i'm on
-	ips, err := getIPs()
-	if err != nil {
-		return fmt.Errorf("unable to get ips: %s", err)
-	}
-
-	for _, i := range ips {
-		if i.IP.Mask(i.Mask).Equal(ip.IP.Mask(i.Mask)) {
-			ip.Mask = i.Mask
-			break
-		}
-	}
-
-	if len(ip.Mask) == 0 {
-		if !ignoreSubnet {
-			return ErrInvalidSubnet
+	if !useDHCP {
+		ips, err := getIPs()
+		if err != nil {
+			return fmt.Errorf("unable to get ips: %s", err)
 		}
 
-		// default to a /24
-		ip.Mask = net.IPv4Mask(255, 255, 255, 0)
+		for _, i := range ips {
+			if i.IP.Mask(i.Mask).Equal(ip.IP.Mask(i.Mask)) {
+				ip.Mask = i.Mask
+				break
+			}
+		}
+
+		if len(ip.Mask) == 0 {
+			if !ignoreSubnet {
+				return ErrInvalidSubnet
+			}
+
+			// default to a /24
+			ip.Mask = net.IPv4Mask(255, 255, 255, 0)
+		}
+
+		fmt.Printf("Using IP: %s\n", ip.String())
+	} else {
+		fmt.Printf("Using DHCP; Not assigning a static address\n")
 	}
 
-	/*
-		// change the hostname
-		if err = changeHostname(hn); err != nil {
-			return fmt.Errorf("failed to change hostname: %w", err)
-		}
-	*/
+	// change the hostname
+	fmt.Printf("Writing hostname...")
+	if err = changeHostname(hn); err != nil {
+		fmt.Printf("\n")
+		return fmt.Errorf("failed to change hostname: %w", err)
+	}
+	fmt.Printf("done.\n")
 
 	// change the ip
-	if err = changeIP(ip); err != nil {
-		return fmt.Errorf("failed to change the ip: %w", err)
+	if !useDHCP {
+		fmt.Printf("Writing static ip...")
+		if err = changeIP(ip); err != nil {
+			fmt.Printf("\n")
+			return fmt.Errorf("failed to change the ip: %w", err)
+		}
+		fmt.Printf("done.\n")
 	}
 
 	return nil
 }
 
 func changeHostname(hn string) error {
-	f, err := os.Create("/etc/hostname")
+	f, err := os.Create(HostnameFile)
 	if err != nil {
-		return fmt.Errorf("failed to open file %q: %w", "/etc/hostname", err)
+		return fmt.Errorf("failed to open file %q: %w", HostnameFile, err)
 	}
 	defer f.Close()
 
@@ -252,29 +388,50 @@ func changeHostname(hn string) error {
 func changeIP(ip *net.IPNet) error {
 	// copy to backup
 
-	/*
-		f, err := os.Open("/etc/dhcpcd.conf", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return fmt.Errorf("failed to open file %q: %w", "/etc/dhcpcd.conf", err)
-		}
-	*/
+	f, err := os.OpenFile(DHCPFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %w", DHCPFile, err)
+	}
+	defer f.Close()
 
 	// TODO get this from the current subnet, just like the mask?
 	// default router address is .1
 	router := ip.IP.Mask(ip.Mask)
 	router = incrementIP(router)
 
-	// TODO interface
 	var str strings.Builder
+
+	// TODO interface name
 	str.WriteString("interface eth0\n")
 	str.WriteString(fmt.Sprintf("static ip_address=%s\n", ip.String()))
 	str.WriteString(fmt.Sprintf("static routers=%s\n", router.String()))
 	str.WriteString("static domain_name_servers=127.0.0.1 10.8.0.19 10.8.0.26\n")
-	fmt.Printf("str:\n%s\n", str.String())
 
-	// TODO write string to file!
+	toWrite := str.String()
+	n, err := f.WriteString(toWrite)
+	switch {
+	case err != nil:
+		return fmt.Errorf("failed to write: %w", err)
+	case len(toWrite) != n:
+		return fmt.Errorf("failed to write: wrote %v/%v bytes", n, len(toWrite))
+	}
 
-	// resolve.conf
+	// update resolve.conf (to use dnsmasq)
+	resolveFile, err := os.Create(ResolveFile)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q: %w", ResolveFile, err)
+	}
+	defer resolveFile.Close()
+
+	resolveWrite := []byte("nameserver    127.0.0.1")
+	n, err = resolveFile.Write(resolveWrite)
+	switch {
+	case err != nil:
+		return fmt.Errorf("failed to write: %w", err)
+	case len(resolveWrite) != n:
+		return fmt.Errorf("failed to write: wrote %v/%v bytes", n, len(resolveWrite))
+	}
+
 	return nil
 }
 
