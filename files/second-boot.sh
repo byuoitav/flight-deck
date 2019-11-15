daab7fba0a10798b1a8fbf461d640242940cf798
Daniel Randall <dgrandall@me.com>
2019-11-15 16:38:25 -0700

    normal second boot

--- files/second-boot.sh
+++ files/second-boot.sh
@@ -3,70 +3,70 @@
 echo "Second boot."
 
 #############################
-mkdir -p /etc/i3
-
-# download i3 config
-until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
-	echo "Unable to download pi setup server"
-	sleep 10
-done
-
-until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.4/pi.tar.gz > /tmp/pi.tar.gz); do
-	echo "Unable to download pi setup server"
-	sleep 10
-done
-
-tar -C /tmp -xzmf /tmp/pi.tar.gz
-
-cd /tmp && ./pi &
-
-sleep 10
-startx
+#mkdir -p /etc/i3
+#
+## download i3 config
+#until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
+#	echo "Unable to download pi setup server"
+#	sleep 10
+#done
+#
+#until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.4/pi.tar.gz > /tmp/pi.tar.gz); do
+#	echo "Unable to download pi setup server"
+#	sleep 10
+#done
+#
+#tar -C /tmp -xzmf /tmp/pi.tar.gz
+#
+#cd /tmp && ./pi &
+#
+#sleep 10
+#startx
 ##############################
 
-# # wait for a deployment
-# until [ -f "/tmp/deployment.log" ]; do
-#     echo "Use the av-cli to float to $(hostname)"
-#     sleep 10
-# done
+# wait for a deployment
+until [ -f "/tmp/deployment.log" ]; do
+    echo "Use the av-cli to float to $(hostname)"
+    sleep 10
+done
 
-# printf "\nYay! I'm floating!\n"
+printf "\nYay! I'm floating!\n"
 
-# source /etc/environment
+source /etc/environment
 
-# # Wait for Salt Env Vars to be set if not already
-# while [ -z $SALT_MASTER_HOST ]; do
-#     echo "Waiting for Salt environment variables to be set"
-#     sleep 5
-#     source /etc/environment
-# done
+# Wait for Salt Env Vars to be set if not already
+while [ -z $SALT_MASTER_HOST ]; do
+    echo "Waiting for Salt environment variables to be set"
+    sleep 5
+    source /etc/environment
+done
 
-# echo "master: $SALT_MASTER_HOST" > /etc/salt/minion
-# echo "master_finger: $SALT_MASTER_FINGER" >> /etc/salt/minion
-# echo "startup_states: 'highstate'" >> /etc/salt/minion
+echo "master: $SALT_MASTER_HOST" > /etc/salt/minion
+echo "master_finger: $SALT_MASTER_FINGER" >> /etc/salt/minion
+echo "startup_states: 'highstate'" >> /etc/salt/minion
 
-# # setup minion id
-# echo $SYSTEM_ID > /etc/salt/minion_id
+# setup minion id
+echo $SYSTEM_ID > /etc/salt/minion_id
 
-# # make changes take effect
-# echo "Starting salt highstate; This should take ~1 minutes"
-# systemctl restart salt-minion
-# sleep 60
+# make changes take effect
+echo "Starting salt highstate; This should take ~1 minutes"
+systemctl restart salt-minion
+sleep 60
 
-# # docker
-# echo "Waiting for deployment to finish (~3 minutes)"
-# sleep 180
+# docker
+echo "Waiting for deployment to finish (~3 minutes)"
+sleep 180
 
-# shutdown -r
+shutdown -r
 
-# systemctl disable pi-setup.service
-# rm -f /byu/.first-boot-done
-# rm -f /byu/setup-started
+systemctl disable pi-setup.service
+rm -f /byu/.first-boot-done
+rm -f /byu/setup-started
 
-# echo "Setup complete! I'll never see you again."
-# echo "I'll reboot soon."
-# sleep 8
+echo "Setup complete! I'll never see you again."
+echo "I'll reboot soon."
+sleep 8
 
-# echo ""
-# echo "bye lol"
-# sleep 2
+echo ""
+echo "bye lol"
+sleep 2
