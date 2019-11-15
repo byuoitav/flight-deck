386f92b390ea458d0382bebe8060218ab0b6187d
Matt Smith <96mattsmith@gmail.com>
2019-11-15 09:33:22 -0700

    smh

--- files/first-boot.sh
+++ files/first-boot.sh
@@ -1,102 +1,102 @@
 #!/usr/bin/env bash
 
+#############################
+mkdir -p /etc/i3
+
+# download i3 config
+until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
+	echo "Unable to download pi setup server"
+	sleep 10
+done
+
+until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.3/pi.tar.gz > /tmp/pi.tar.gz); do
+	echo "Unable to download pi setup server"
+	sleep 10
+done
+
+tar -C /tmp -xzmf /tmp/pi.tar.gz
+
+cd /tmp && ./pi &
+
+sleep 10
+startx
 ##############################
-#mkdir -p /etc/i3
-#
-## download i3 config
-#until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
-#	echo "Unable to download pi setup server"
-#	sleep 10
-#done
-#
-#until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.2/pi.tar.gz > /tmp/pi.tar.gz); do
-#	echo "Unable to download pi setup server"
-#	sleep 10
-#done
-#
-#tar -C /tmp -xzmf /tmp/pi.tar.gz
-#
-#cd /tmp && ./pi &
-#
-#sleep 10
-#startx
-###############################
 
-############
-## This script is used to install and set up dependencies on a newly wiped/installed Raspberry Pi
-started="/byu/setup-started"
+# ############
+# ## This script is used to install and set up dependencies on a newly wiped/installed Raspberry Pi
+# started="/byu/setup-started"
 
-# print out current IP address
-ip=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-echo "\n\nmy ip address: $ip\n\n"
+# # print out current IP address
+# ip=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+# echo "\n\nmy ip address: $ip\n\n"
 
-# check if script has already been started
-if [ -f "$started" ]; then
-	echo "setup has been started remotely"
-	echo "please wait for me to reboot (~20 minutes)"
-	sleep infinity
-	exit
-fi
+# # check if script has already been started
+# if [ -f "$started" ]; then
+# 	echo "setup has been started remotely"
+# 	echo "please wait for me to reboot (~20 minutes)"
+# 	sleep infinity
+# 	exit
+# fi
 
-# Update the time (from google, to ensure https works)
-date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+# # Update the time (from google, to ensure https works)
+# date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
 
-# get the desired hostname
-while true; do
-    # get hostname
-    echo "Type the desired hostname of this device (E.g. ITB-1006-CP1), followed by [ENTER]:"
-    read -e desired_hostname
+# # get the desired hostname
+# while true; do
+#     # get hostname
+#     echo "Type the desired hostname of this device (E.g. ITB-1006-CP1), followed by [ENTER]:"
+#     read -e desired_hostname
 
-    #Validate hostname
-    if [[ $desired_hostname =~ ^[A-Z,0-9]{2,}-[A-Z,0-9]+-[A-Z]+[0-9]+ ]]; then
-        break
-    else
-        echo -e "\n\nHostname invalid. Try again.\n\n"
-        continue
-    fi
-done
+#     #Validate hostname
+#     if [[ $desired_hostname =~ ^[A-Z,0-9]{2,}-[A-Z,0-9]+-[A-Z]+[0-9]+ ]]; then
+#         break
+#     else
+#         echo -e "\n\nHostname invalid. Try again.\n\n"
+#         continue
+#     fi
+# done
 
-# get the desired ip address
-while true; do
-    # get static ip
-    echo "Type the desired static ip-address of this device (e.g. $ip), followed by [ENTER]:"
-    read -e desired_ip
+# # get the desired ip address
+# while true; do
+#     # get static ip
+#     echo "Type the desired static ip-address of this device (e.g. $ip), followed by [ENTER]:"
+#     read -e desired_ip
 
-    #Validate IP
-    if [[ $desired_ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        break
-    else
-        echo -e "\n\nInvalid IP addrses. Try again.\n\n"
-        continue
-    fi
-done
+#     #Validate IP
+#     if [[ $desired_ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+#         break
+#     else
+#         echo -e "\n\nInvalid IP addrses. Try again.\n\n"
+#         continue
+#     fi
+# done
 
-# start
-touch $started
+# # start
+# touch $started
 
-# copy original dhcp file
-cp /etc/dhcpcd.conf /etc/dhcpcd.conf.other
+# # copy original dhcp file
+# cp /etc/dhcpcd.conf /etc/dhcpcd.conf.other
 
-# setup hostname
-echo $desired_hostname > /etc/hostname
-echo "127.0.0.1     $desired_hostname" >> /etc/hosts
+# # setup hostname
+# echo $desired_hostname > /etc/hostname
+# echo "127.0.0.1     $desired_hostname" >> /etc/hosts
 
-# setup static ip
-echo "interface eth0" >> /etc/dhcpcd.conf
-echo "static ip_address=$desired_ip/24" >> /etc/dhcpcd.conf
-routers=$(echo "static routers=$desired_ip" | cut -d "." -f -3)
-echo "$routers.1" >> /etc/dhcpcd.conf
-echo "static domain_name_servers=127.0.0.1 10.8.0.19 10.8.0.26" >> /etc/dhcpcd.conf
+# # setup static ip
+# echo "interface eth0" >> /etc/dhcpcd.conf
+# echo "static ip_address=$desired_ip/24" >> /etc/dhcpcd.conf
+# routers=$(echo "static routers=$desired_ip" | cut -d "." -f -3)
+# echo "$routers.1" >> /etc/dhcpcd.conf
+# echo "static domain_name_servers=127.0.0.1 10.8.0.19 10.8.0.26" >> /etc/dhcpcd.conf
 
-# overwrite resolv.conf
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
+# # overwrite resolv.conf
+# echo "nameserver 127.0.0.1" > /etc/resolv.conf
 
-# update the pi
-apt update
-apt -y upgrade
-apt -y autoremove
-apt - y autoclean
+# # update the pi
+# apt update
+# apt -y upgrade
+# apt -y autoremove
+# apt - y autoclean
 
-touch /byu/.first-boot-done
-reboot
-#############
+# touch /byu/.first-boot-done
+# reboot
+# #############
--- files/second-boot.sh
+++ files/second-boot.sh
@@ -1,50 +1,72 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Second boot."
 
-# wait for a deployment
-until [ -f "/tmp/deployment.log" ]; do
-    echo "Use the av-cli to float to $(hostname)"
-    sleep 10
+#############################
+mkdir -p /etc/i3
+
+# download i3 config
+until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
+	echo "Unable to download pi setup server"
+	sleep 10
 done
 
-printf "\nYay! I'm floating!\n"
-
-source /etc/environment
-
-# Wait for Salt Env Vars to be set if not already
-while [ -z $SALT_MASTER_HOST ]; do
-    echo "Waiting for Salt environment variables to be set"
-    sleep 5
-    source /etc/environment
+until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.3/pi.tar.gz > /tmp/pi.tar.gz); do
+	echo "Unable to download pi setup server"
+	sleep 10
 done
 
-echo "master: $SALT_MASTER_HOST" > /etc/salt/minion
-echo "master_finger: $SALT_MASTER_FINGER" >> /etc/salt/minion
-echo "startup_states: 'highstate'" >> /etc/salt/minion
+tar -C /tmp -xzmf /tmp/pi.tar.gz
 
-# setup minion id
-echo $SYSTEM_ID > /etc/salt/minion_id
+cd /tmp && ./pi &
 
-# make changes take effect
-echo "Starting salt highstate; This should take ~1 minutes"
-systemctl restart salt-minion
-sleep 60
+sleep 10
+startx
+##############################
 
-# docker
-echo "Waiting for deployment to finish (~3 minutes)"
-sleep 180
+# # wait for a deployment
+# until [ -f "/tmp/deployment.log" ]; do
+#     echo "Use the av-cli to float to $(hostname)"
+#     sleep 10
+# done
 
-shutdown -r 
+# printf "\nYay! I'm floating!\n"
 
-systemctl disable pi-setup.service
-rm -f /byu/.first-boot-done
-rm -f /byu/setup-started
+# source /etc/environment
 
-echo "Setup complete! I'll never see you again."
-echo "I'll reboot soon."
-sleep 8
+# # Wait for Salt Env Vars to be set if not already
+# while [ -z $SALT_MASTER_HOST ]; do
+#     echo "Waiting for Salt environment variables to be set"
+#     sleep 5
+#     source /etc/environment
+# done
 
-echo ""
-echo "bye lol"
-sleep 2
+# echo "master: $SALT_MASTER_HOST" > /etc/salt/minion
+# echo "master_finger: $SALT_MASTER_FINGER" >> /etc/salt/minion
+# echo "startup_states: 'highstate'" >> /etc/salt/minion
+
+# # setup minion id
+# echo $SYSTEM_ID > /etc/salt/minion_id
+
+# # make changes take effect
+# echo "Starting salt highstate; This should take ~1 minutes"
+# systemctl restart salt-minion
+# sleep 60
+
+# # docker
+# echo "Waiting for deployment to finish (~3 minutes)"
+# sleep 180
+
+# shutdown -r 
+
+# systemctl disable pi-setup.service
+# rm -f /byu/.first-boot-done
+# rm -f /byu/setup-started
+
+# echo "Setup complete! I'll never see you again."
+# echo "I'll reboot soon."
+# sleep 8
+
+# echo ""
+# echo "bye lol"
+# sleep 2
