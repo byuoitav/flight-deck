b5303e2857492f9da29943cca515ec1f0cf77f84
Daniel Randall <dgrandall@me.com>
2019-11-18 15:09:10 -0700

    ready to test the whole thing (just about)

--- files/setup.sh
+++ files/setup.sh
@@ -4,48 +4,48 @@ echo ""
 echo "Hi From Danny"
 echo ""
 
-# mkdir -p /etc/i3
-#
-# # download i3 config
-# until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
-# 	echo "Unable to download pi setup server"
-# 	sleep 10
-# done
-#
-# # download champagne
-# until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.4/pi.tar.gz > /tmp/pi.tar.gz); do
-# 	echo "Unable to download pi setup server"
-# 	sleep 10
-# done
-#
-# tar -C /tmp -xzmf /tmp/pi.tar.gz
-#
-# # log to champagne.log
-# exec > /tmp/champagne.log 2>&1
-#
-# cd /tmp && ./pi &
-#
-# sleep 4
-# startx
+mkdir -p /etc/i3
 
-if [ ! -f "/byu/.first-boot-done" ]; then
-	echo "First boot."
+# download i3 config
+until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
+	echo "Unable to download pi setup server"
+	sleep 10
+done
 
-	until $(curl https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/first-boot.sh > /tmp/first-boot.sh); do
-		echo "Unable to download first-boot.sh - Trying again in 5 seconds."
-        sleep 5
-	done
-	chmod +x /tmp/first-boot.sh
+# download champagne
+until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.4/pi.tar.gz > /tmp/pi.tar.gz); do
+	echo "Unable to download pi setup server"
+	sleep 10
+done
 
-	/tmp/first-boot.sh
-else
-	echo "Second boot."
+tar -C /tmp -xzmf /tmp/pi.tar.gz
 
-	until $(curl https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/second-boot.sh > /tmp/second-boot.sh); do
-		echo "Unable to download second-boot.sh - Trying again in 5 seconds."
-        sleep 5
-	done
-	chmod +x /tmp/second-boot.sh
+# log to champagne.log
+exec > /tmp/champagne.log 2>&1
 
-	/tmp/second-boot.sh
-fi
+cd /tmp && ./pi &
+
+sleep 4
+startx
+
+# if [ ! -f "/byu/.first-boot-done" ]; then
+# 	echo "First boot."
+#
+# 	until $(curl https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/first-boot.sh > /tmp/first-boot.sh); do
+# 		echo "Unable to download first-boot.sh - Trying again in 5 seconds."
+#         sleep 5
+# 	done
+# 	chmod +x /tmp/first-boot.sh
+#
+# 	/tmp/first-boot.sh
+# else
+# 	echo "Second boot."
+#
+# 	until $(curl https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/second-boot.sh > /tmp/second-boot.sh); do
+# 		echo "Unable to download second-boot.sh - Trying again in 5 seconds."
+#         sleep 5
+# 	done
+# 	chmod +x /tmp/second-boot.sh
+#
+# 	/tmp/second-boot.sh
+# fi
