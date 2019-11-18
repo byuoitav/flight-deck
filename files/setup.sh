db91ca6ec0f4e74878d53644ee60fed6751612eb
Daniel Randall <dgrandall@me.com>
2019-11-18 16:36:36 -0700

    bump version

--- files/setup.sh
+++ files/setup.sh
@@ -13,7 +13,7 @@ until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master
 done
 
 # download champagne
-until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.6/pi.tar.gz > /tmp/pi.tar.gz); do
+until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.7/pi.tar.gz > /tmp/pi.tar.gz); do
 	echo "Unable to download champagne"
 	sleep 10
 done
