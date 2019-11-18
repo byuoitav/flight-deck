35fd2a8c9044bb292a7d9d6b547ce9bbab606727
Daniel Randall <dgrandall@me.com>
2019-11-18 15:30:37 -0700

    better name

--- files/setup.sh
+++ files/setup.sh
@@ -8,13 +8,13 @@ mkdir -p /etc/i3
 
 # download i3 config
 until $(curl -fsSL https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setupi3config > /etc/i3/config); do
-	echo "Unable to download pi setup server"
+	echo "Unable to download i3 config file"
 	sleep 10
 done
 
 # download champagne
 until $(curl -fsSL https://github.com/byuoitav/flight-deck/releases/download/v0.1.6/pi.tar.gz > /tmp/pi.tar.gz); do
-	echo "Unable to download pi setup server"
+	echo "Unable to download champagne"
 	sleep 10
 done
 
