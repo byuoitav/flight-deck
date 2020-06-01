0a6e9218d05196027594b387a022bd9e115a60db
Brayden Winterton <bwinterton@gmail.com>
2020-06-01 15:27:31 -0600

    Make image setup script ignore certs on setup script pull
    This change allows the pis to still pull in the setup script even if
    they have incorrect dates set which would cause the certificate checks
    to fail.

--- image/pi-setup.sh
+++ image/pi-setup.sh
@@ -4,7 +4,7 @@ sleep 5
 chvt 2
 
 # try to download the setup script
-until $(curl https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setup.sh > /tmp/setup.sh); do
+until $(curl --insecure https://raw.githubusercontent.com/byuoitav/flight-deck/master/files/setup.sh > /tmp/setup.sh); do
     echo "Unable to download setup script; Trying again in 5 seconds."
     sleep 5
 done
