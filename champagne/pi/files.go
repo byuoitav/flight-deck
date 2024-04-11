ad91c645d8defb007acafd52bcba0502c70af508
Clinton Reeder <clinton_reeder@byu.edu>
2024-04-11 13:26:11 -0600

    additional fixes to files.go

--- champagne/pi/files.go
+++ champagne/pi/files.go
@@ -126,7 +126,7 @@ func changeIP(ip *net.IPNet) error {
 		ipv4dnsName := "ipv4.dns"
 		ipv4dns := "127.0.0.1,10.8.0.19,10.8.0.26"
 
-		cmd := exec.Command(perm, app, connmod, strconv.Quote(profile), ipv4addName, ipv4add, ipv4gateway, ipv4method, ipv4dns)
+		cmd := exec.Command(perm, app, connmod, strconv.Quote(profile), ipv4addName, ipv4add, ipvgatewayName, ipv4gateway, ipv4methodName, ipv4method, ipv4dnsName, ipv4dns)
 		log.Printf("Command: %s\n", cmd.String())
 		out, err := cmd.Output()
 		if err != nil {
